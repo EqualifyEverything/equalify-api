b864b63e9ed9d3cab40e16ab72fd5082cfa0948e
Christopher Aitken <chris@heythisischris.com>
2024-04-15 21:58:21 -0500

    Now supporting local dev, added example env, configured fastifyPg plugin

--- app.ts
+++ app.ts
@@ -1,8 +1,19 @@
-import Fastify from "fastify";
+import Fastify from 'fastify';
+import fastifyPg from '@fastify/postgres';
+export const fastify = Fastify();
 
-export const app = Fastify();
+fastify.register(fastifyPg, { connectionString: process.env.DB_CONNECTION_URL });
 
-app.get("/", async (request, reply) => {
+fastify.get('/', async (request, reply) => {
+    const client = await fastify.pg.connect()
+    const { rows } = await client.query(`SELECT * FROM "users"`);
+    console.log(rows);
+    await client.release();
     console.log(request, reply);
-    return { message: "Equalify everything!" };
+    return { message: 'Equalify everything!' };
 });
+
+fastify.listen({ port: 3000 }, (err) => {
+    console.log(`Server listening on ${fastify.server.address().port}`)
+    if (err) throw err
+})
--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import awsLambdaFastify from "@fastify/aws-lambda";
-import { app } from "./app";
-
-const proxy = awsLambdaFastify(app);
+import { fastify } from "./app";
+const proxy = awsLambdaFastify(fastify);
 
 export async function handler(event: any, context: any) {
     return await proxy(event, context);
