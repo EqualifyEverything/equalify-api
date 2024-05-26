2a2e0a7c416dc3b8956ed3bde24c44cdea53815e
Christopher Aitken <chris@heythisischris.com>
2024-05-26 00:15:17 -0500

    Added createdAt/updatedAt to properties, worked on addScans/addResults

--- src/routes/addResults.ts
+++ src/routes/addResults.ts
@@ -1,8 +1,16 @@
 import { jwtClaims } from '../app.js';
-import { pgClient, validateUuid } from '../utils/index.js';
+import { formatId, pgClient } from '../utils/index.js';
 
 export const addResults = async ({ request, reply }) => {
     await pgClient.connect();
+    const id = (await pgClient.query(`
+        INSERT INTO "results" ("user_id") VALUES ($1) RETURNING "id"
+    `, [jwtClaims.sub])).rows?.[0]?.id;
     await pgClient.clean();
-    return;
+
+    return {
+        status: 'success',
+        message: 'Result added successfully',
+        result: formatId(id),
+    };
 }
--- src/routes/addScans.ts
+++ src/routes/addScans.ts
@@ -1,8 +1,23 @@
 import { jwtClaims } from '../app.js';
-import { pgClient, validateUuid } from '../utils/index.js';
+import { formatId, pgClient } from '../utils/index.js';
 
 export const addScans = async ({ request, reply }) => {
+    if (!(request.body.propertyIds || request.body.urlIds)) {
+        return {
+            status: 'error',
+            message: 'propertyIds or urlIds required.',
+        }
+    }
+
     await pgClient.connect();
+    const id = (await pgClient.query(`
+        INSERT INTO "scans" ("user_id", "property_ids", "url_ids") VALUES ($1, $2, $3) RETURNING "id"
+    `, [jwtClaims.sub, request.body.propertyIds, request.body.urlIds])).rows?.[0]?.id;
     await pgClient.clean();
-    return;
+
+    return {
+        status: 'success',
+        message: 'Scan added successfully',
+        result: formatId(id),
+    };
 }
