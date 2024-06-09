cd5398f3c755f721c6c41927e185e65b7fd0ed32
Christopher Aitken <chris@heythisischris.com>
2024-06-09 02:40:09 -0500

    Fixed verification code message

--- src/cognito/customMessageResendCode.ts
+++ src/cognito/customMessageResendCode.ts
@@ -1,7 +1,7 @@
 import { formatEmail } from "../utils";
 
 export const customMessageResendCode = async (event) => {
-    event.response.emailSubject = `Confirm your registration to Equalify`;
-    event.response.emailMessage = formatEmail({ body: `<p>${event.request.userAttributes['name']},</p><p>Please confirm your registration by <a href="https://${process.env.STAGING ? 'staging.' : ''}equalify.dev/login?username=${event.userName}&code=${event.request.codeParameter}&email=${event.request.userAttributes.email}">clicking here</a>.<p>Thank you,<br/>Equalify<div style="display:none"><a>${event.request.codeParameter}</a><a>${event.request.codeParameter}</a></div>` });
+    event.response.emailSubject = `Equalify Verification Code`;
+    event.response.emailMessage = formatEmail({ body: `<p>Hey there,</p><p>Your Equalify verification code is ${event.request.codeParameter}.</p><p>Thank you,<br/>Equalify` });
     return event;
 }
