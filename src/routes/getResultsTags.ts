034928ae41d3be8af6b30b5218c70ec0544e7b24
Christopher Aitken <chris@heythisischris.com>
2024-07-10 02:20:41 -0500

    Finished getResultsTags & getResultsUrls

--- src/routes/getResultsTags.ts
+++ src/routes/getResultsTags.ts
@@ -1,5 +1,84 @@
 import { graphqlQuery, db } from '#src/utils';
 
 export const getResultsTags = async ({ request, reply }) => {
-    return;
+    await db.connect();
+    const report = (await db.query(`SELECT "id", "name", "filters" FROM "reports" WHERE "id" = $1`, [request.query.reportId])).rows?.[0];
+    const types = ['properties', 'urls', 'messages', 'nodes', 'tags'];
+    const filters = Object.fromEntries(types.map(obj => [obj, []]));
+    for (const type of types) {
+        filters[type] = report.filters.filter(obj => obj.type === type).map(obj => obj.value)
+    }
+    const urls = (await db.query({
+        text: `SELECT "id", "url" FROM "urls" WHERE "id" = ANY($1::uuid[]) OR "property_id" = ANY($2::uuid[])`,
+        values: [filters.urls, filters.properties],
+    })).rows;
+    await db.clean();
+
+    const response = await graphqlQuery({
+        query: `query($urlIds: [UUID!], $tagId: UUID!){
+            nodes: enodes(filter: { urlId: { in: $urlIds } }) {
+                nodeId: id
+                createdAt
+                html
+                targets
+                relatedUrlId: urlId
+                equalified
+                messageNodes {
+                    id
+                    node: enode {equalified}
+                    message {
+                        id
+                        message
+                        messageTags {
+                            tag {id tag}
+                        }
+                    }
+                }
+            }
+            tag(id: $tagId) {id tag}
+        }`,
+        variables: { urlIds: urls.map(obj => obj.id), tagId: request.query.tagId },
+    });
+
+    const filteredNodes = response.data.nodes
+        .filter(obj => obj.messageNodes.map(obj => obj.message.messageTags.map(obj => obj.tag_id)).flat().includes(request.query.messageId));
+    const formattedMessages = {};
+    for (const message of filteredNodes.map(obj => obj.messageNodes).flat()) {
+        if (!formattedMessages?.[message.message.id]) {
+            formattedMessages[message.message.id] = {
+                id: message.message.id,
+                messageId: message.message.id,
+                message: message.message.message,
+                equalifiedCount: 0,
+                activeCount: 0,
+            };
+        }
+        formattedMessages[message.message.id][message.node.equalified ? 'equalifiedCount' : 'activeCount'] += 1;
+    }
+
+    const formattedChart = {};
+    for (const node of filteredNodes) {
+        const date = node.createdAt.split('T')[0];
+        if (!formattedChart?.[date]) {
+            formattedChart[date] = {
+                date: date,
+                equalified: 0,
+                active: 0,
+            };
+        }
+        formattedChart[date][node.equalified ? 'equalified' : 'active'] += 1;
+    }
+
+    return {
+        reportName: report.name,
+        tagName: response.data.tag.tag,
+        messages: Object.values(formattedMessages)
+            .sort((a, b) => a.activeCount > b.activeCount ? -1 : 1)
+            .map(obj => ({
+                ...obj,
+                totalCount: obj.equalifiedCount + obj.activeCount,
+            })),
+        chart: Object.values(formattedChart)
+            .sort((a, b) => a.date > b.date ? -1 : 1),
+    };
 }
--- src/routes/getResultsUrls.ts
+++ src/routes/getResultsUrls.ts
@@ -1,5 +1,54 @@
 import { graphqlQuery, db } from '#src/utils';
 
 export const getResultsUrls = async ({ request, reply }) => {
-    return;
+    await db.connect();
+    const report = (await db.query(`SELECT "id", "name", "filters" FROM "reports" WHERE "id" = $1`, [request.query.reportId])).rows?.[0];
+    const types = ['properties', 'urls', 'messages', 'nodes', 'tags'];
+    const filters = Object.fromEntries(types.map(obj => [obj, []]));
+    for (const type of types) {
+        filters[type] = report.filters.filter(obj => obj.type === type).map(obj => obj.value)
+    }
+    await db.clean();
+
+    // Make a request
+    const response = await graphqlQuery({
+        query: `query($urlId: UUID!){
+            nodes: enodes(filter: { urlId: { equalTo: $urlId } }) {
+                createdAt
+                html
+                equalified
+                messageNodes {message{id message}}
+            }
+            url(id: $urlId) {id url}
+        }`,
+        variables: { urlId: request.query.urlId },
+    });
+
+    const formattedChart = {};
+    for (const node of response.data.nodes) {
+        const date = node.createdAt.split('T')[0];
+        if (!formattedChart?.[date]) {
+            formattedChart[date] = {
+                date: date,
+                equalified: 0,
+                active: 0,
+            };
+        }
+        formattedChart[date][node.equalified ? 'equalified' : 'active'] += 1;
+    }
+
+    return {
+        reportName: report.name,
+        url: response.data.url.url,
+        urlId: response.data.url.id,
+        nodes: response.data.nodes.map(obj => ({
+            createdAt: obj.createdAt,
+            message: obj.messageNodes?.[0]?.message?.message,
+            messageId: obj.messageNodes?.[0]?.message?.id,
+            codeSnippet: obj.html,
+            status: obj.equalified ? 'Equalified' : 'Active',
+        })).sort((a, b) => a.createdAt < b.createdAt ? -1 : 1),
+        chart: Object.values(formattedChart)
+            .sort((a, b) => a.date > b.date ? -1 : 1),
+    };
 }
