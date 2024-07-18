86a22b41f8875319f625c3766902e09f3510600a
Christopher Aitken <chris@heythisischris.com>
2024-07-18 14:42:08 -0500

    Updated reset password email message

--- src/cognito/customMessageForgotPassword.ts
+++ src/cognito/customMessageForgotPassword.ts
@@ -2,6 +2,6 @@ import { formatEmail } from '#src/utils';
 
 export const customMessageForgotPassword = async (event) => {
     event.response.emailSubject = `Please reset your password`;
-    event.response.emailMessage = formatEmail({ body: `<p>${event.request.userAttributes['name']},</p><p>Please reset your password by <a href="https://${process.env.STAGING ? 'staging.' : ''}equalify.dev/reset?username=${event.userName}&code=${event.request.codeParameter}">clicking here</a>.<p>Thank you,<br/>Equalify<div style="display:none"><a>${event.request.codeParameter}</a><a>${event.request.codeParameter}</a></div>` });
+    event.response.emailMessage = formatEmail({ body: `<p>${event.request.userAttributes['name'] ?? 'Hello there'},</p><p>Please reset your password by <a href="https://${process.env.STAGING ? 'staging.' : ''}equalify.dev/reset?username=${event.userName}&code=${event.request.codeParameter}">clicking here</a>.<p>Thank you,<br/>Equalify<div style="display:none"><a>${event.request.codeParameter}</a><a>${event.request.codeParameter}</a></div>` });
     return event;
 }
