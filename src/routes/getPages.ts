61e9353573e6de98f4d81b2285602d15e0ce9d2a
azdak <negatia@gmail.com>
2024-10-18 16:24:12 -0500

    Update getPages fields and flatten output

--- src/routes/getPages.ts
+++ src/routes/getPages.ts
@@ -18,14 +18,27 @@ export const getPages = async ({ request, reply }) => {
                 }
         `,
         variables: {
-            limit: parseInt(request.query.limit ?? 100),
+            limit: parseInt(request.query.limit ?? 50),
             offset: parseInt(request.query.offset ?? 0),
         },
     });
 
     return {
         status: 'success',
-        result: response,
+        result: response.map(
+            ({
+                url,
+                id, 
+                property:
+                { 
+                    id:property_id, 
+                    name:property_name 
+                }, 
+                scans:
+                {
+                    updated_at:last_scanned
+                }
+            })=>({ url, id, property_name, property_id, last_scanned })),
         total: response?.count,
     };
 }
