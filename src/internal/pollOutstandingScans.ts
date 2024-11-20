59784b1672ddbcbbf69fcc040129970803a056c8
azdak <negatia@gmail.com>
2024-11-19 20:17:34 -0600

    pollOutstandingScans testing

--- src/internal/pollOutstandingScans.ts
+++ src/internal/pollOutstandingScans.ts
@@ -3,7 +3,7 @@ export const pollOutstandingScans = async ({ request, reply }) => {
     console.log(`START POLLING OPEN SCANS`);
     const startTime = new Date().getTime();
     const jobIds = (await db.query({
-        text: `SELECT job_id FROM scans WHERE s.processing = TRUE`
+        text: `SELECT job_id FROM scans WHERE processing = TRUE`
     })).rows.map(obj => obj.job_id);
     await db.clean();
     const deltaTime = new Date().getTime() - startTime;
