7cc30c18868aa0eb8e9d60ce207db7372581b61d
azdak <negatia@gmail.com>
2024-11-24 18:11:12 -0600

    Update addScansByPage.ts

--- src/routes/addScansByPage.ts
+++ src/routes/addScansByPage.ts
@@ -24,7 +24,7 @@ urls:
 
 export const addScansByPage = async ({ request, reply }) => {
 
-  const req = JSON.parse(request).body;
+  const req = JSON.parse(request.body);
   // check request
   if (!req.urls) {
     console.log(JSON.stringify(req));
