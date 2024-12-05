b5a3ca1137323b9403526160c23475f4e598dee1
azdak <negatia@gmail.com>
2024-12-04 22:13:48 -0600

    Update getPage.ts

--- src/routes/getPage.ts
+++ src/routes/getPage.ts
@@ -19,8 +19,6 @@ export const getPage = async ({ request, reply }) => {
 
     return {
         status: 'success',
-        result: response?.url_by_id.map(obj => ({
-            ...obj,
-        })),
+        result: response,
     };
 }
