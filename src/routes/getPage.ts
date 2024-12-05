75bc7c41d9f531cfb31cf36b26e296b912bb80a7
azdak <negatia@gmail.com>
2024-12-04 22:17:24 -0600

    Update getPage.ts

--- src/routes/getPage.ts
+++ src/routes/getPage.ts
@@ -4,12 +4,21 @@ export const getPage = async ({ request, reply }) => {
     const response = await graphql({
         request,
         query: `query($id: uuid!){
-            url_by_id(id:$id) { 
+            url_by_id(id:$id) {
+               id 
                url
                created_at
                updated_at
-               property { id name }
-               scans { created_at updated_at processing }
+               property { 
+                    id 
+                    name 
+                }
+               scans { 
+                    id 
+                    created_at 
+                    updated_at 
+                    processing 
+                }
             }
         }`,
         variables: {
