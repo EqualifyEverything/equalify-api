c0303b794e9af445b8b905e8aaa5b250bc1337a5
azdak <negatia@gmail.com>
2024-12-04 22:04:59 -0600

    Added getPage endpoint

---
+++ src/routes/getPage.ts
@@ -0,0 +1,24 @@
+import { graphql } from '#src/utils';
+
+export const getPage = async ({ request, reply }) => {
+    const response = await graphql({
+        request,
+        query: `query($id: uuid!){
+            url_by_id(id:$id) { 
+               url
+               created_at
+               updated_at
+               property { id name }
+               scans { created_at updated_at processing }
+            }
+        }`,
+        variables: {
+            id: request.query.pageId,
+        },
+    });
+
+    return {
+        status: 'success',
+        result: response?.url_by_id,
+    };
+}
