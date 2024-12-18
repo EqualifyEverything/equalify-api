b87f37267eaf4d7358855b510895548f5304b1b9
azdak <negatia@gmail.com>
2024-12-18 16:48:26 -0600

    Update addScansByProperty.ts

--- src/routes/addScansByProperty.ts
+++ src/routes/addScansByProperty.ts
@@ -4,44 +4,38 @@ import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
 const lambda = new LambdaClient();
 
 export const addScansByProperty = async ({ request, reply }) => {
-  if (!request.body.propertyIds) {
+  if (!request.body.propertyId) {
     return {
       status: "error",
-      message: "PropertyIds are required.",
+      message: "PropertyId is required.",
     };
   }
 
-  for(const propertyId of request.body.propertyIds){
-    if(!validateUuid(propertyId)) return {
-        status: "error",
-        message: `${propertyId} is not a valid of UUID.`
-      }; 
-  }
+  if (!validateUuid(request.body.propertyId))
+    return {
+      status: "error",
+      message: `${request.body.propertyId} is not a valid of UUID.`,
+    };
 
-  await db.connect();
+  //await db.connect();
 
-  let urlsToScan = [];
-  for(const propertyId of request.body.propertyIds){
-    const urls = (
-        await graphql({
-        request,
-        query: `{
-            urls(where: {property_id: {_eq: $propertyId}}) {
-                urlId: id
-                url
-            }
-        }`,
-        variables: {
-          propertyId: propertyId,
-        },
-    })).urls;
-    if(urls) urlsToScan.push(urls);
-  }
-  
-  await db.clean();
+  const urls = await graphql({
+    request,
+    query: `{
+        urls(where: {property_id: {_eq: $propertyId}}) {
+            urlId: id
+            url
+        }
+    }`,
+    variables: {
+      propertyId: request.body.propertyId,
+    },
+  });
+
+  //await db.clean();
 
   return {
     status: "success",
-    message: urlsToScan,
+    message: urls,
   };
 };
