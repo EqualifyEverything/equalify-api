21734b7902ea422bb1c176cae613f96738a92242
azdak <negatia@gmail.com>
2024-12-18 13:05:43 -0600

    Removed "old" comparison in getFilters query

--- src/routes/getFilters.ts
+++ src/routes/getFilters.ts
@@ -4,11 +4,11 @@ export const getFilters = async ({ request, reply }) => {
     const response = await graphql({
         request,
         query: `{
-            messages(where: {old:{_eq: false}}) {
+            messages {
                 value: id
                 label: message
             }
-            tags(where: {old:{_eq: false}}) {
+            tags {
                 value: id
                 label: tag
             }
@@ -26,10 +26,10 @@ export const getFilters = async ({ request, reply }) => {
     return {
         status: 'success',
         result: {
-            messages: response.messages?.map(obj => ({ ...obj, type: 'messages' })),
-            tags: response.tags?.map(obj => ({ ...obj, type: 'tags' })),
-            properties: response.properties?.map(obj => ({ ...obj, type: 'properties' })),
-            urls: response.urls?.map(obj => ({ ...obj, type: 'urls' })),
+            messages: response.messages.map(obj => ({ ...obj, type: 'messages' })),
+            tags: response.tags.map(obj => ({ ...obj, type: 'tags' })),
+            properties: response.properties.map(obj => ({ ...obj, type: 'properties' })),
+            urls: response.urls.map(obj => ({ ...obj, type: 'urls' })),
             statuses: [
                 { label: 'Active', value: 'active', type: 'status' },
                 { label: 'Equalified', value: 'equalified', type: 'status' },
