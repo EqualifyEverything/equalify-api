b2a5482474a29613e14f3b0d4142ca2aa228a609
azdak <negatia@gmail.com>
2024-12-20 12:35:51 -0600

    removed url and discovery fields from /add/properties

--- src/routes/addProperties.ts
+++ src/routes/addProperties.ts
@@ -8,35 +8,40 @@ export const addProperties = async ({ request, reply }) => {
             message: 'Property Name is required.',
         }
     }
-    else if (!request.body.propertyUrl) {
+    /* else if (!request.body.propertyUrl) {
         return {
             status: 'error',
             message: 'Property URL is required.',
         }
-    }
-    else if (!request.body.propertyDiscovery) {
+    } */
+    /* else if (!request.body.propertyDiscovery) {
         return {
             status: 'error',
             message: 'Property Discovery settings are required.',
         }
-    }
-    else if (!validateUrl(request.body.propertyUrl)) {
+    } */
+    /* else if (!validateUrl(request.body.propertyUrl)) {
         return {
             status: 'error',
             message: 'Property URL is not valid.',
         }
-    }
-    else if (!validateDiscovery(request.body.propertyDiscovery)) {
+    } */
+    /* else if (!validateDiscovery(request.body.propertyDiscovery)) {
         return {
             status: 'error',
             message: `Property Discovery is not valid- must be one of: ${validateDiscoveryOptions.join(', ')}`,
         }
-    }
+    } */
 
     await db.connect();
     const id = (await db.query(`
-        INSERT INTO "properties" ("user_id", "name", "discovery", "property_url") VALUES ($1, $2, $3, $4) RETURNING "id"
-    `, [jwtClaims.sub, request.body.propertyName, request.body.propertyDiscovery, `${request.body.propertyUrl}${(request.body.propertyDiscovery === 'single' && !request.body.propertyUrl.endsWith('/')) ? '/' : ''}`])).rows?.[0]?.id;
+        INSERT INTO "properties" ("user_id", "name") VALUES ($1, $2, $3, $4) RETURNING "id"
+    `, [
+        jwtClaims.sub, 
+        request.body.propertyName, 
+        //request.body.propertyDiscovery, 
+        //`${request.body.propertyUrl}${(request.body.propertyDiscovery === 'single' && !request.body.propertyUrl.endsWith('/')) ? '/' : ''}`
+    ])).rows?.[0]?.id;
     await db.clean();
 
     return {
