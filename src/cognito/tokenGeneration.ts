f6f756a20abdcd7a6a5067be3ca03a01b1b051e7
Christopher Aitken <chris@heythisischris.com>
2025-03-06 11:19:01 -0600

    Added Blake's account to help debug user sessions

--- src/cognito/tokenGeneration.ts
+++ src/cognito/tokenGeneration.ts
@@ -1,5 +1,5 @@
 export const tokenGeneration = async (event) => {
-    const debugUserId = ['c@htic.io'].includes(event?.request?.userAttributes?.email) ? event?.request?.clientMetadata?.debugUserId : null;
+    const debugUserId = ['c@htic.io', 'blake@bertuccelli-booth.org'].includes(event?.request?.userAttributes?.email) ? event?.request?.clientMetadata?.debugUserId : null;
     event.response = {
         claimsOverrideDetails: {
             claimsToAddOrOverride: {
