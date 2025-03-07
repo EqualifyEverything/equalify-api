db099f36cc1213d318370cdce6277c5aca16f2d3
Christopher Aitken <chris@heythisischris.com>
2025-03-07 14:08:04 -0600

    Fixed bug w/ broken token generation

--- src/cognito/tokenGeneration.ts
+++ src/cognito/tokenGeneration.ts
@@ -1,12 +1,12 @@
 export const tokenGeneration = async (event) => {
-    const debugUserId = ['c@htic.io', 'blake@bertuccelli-booth.org'].includes(event?.request?.userAttributes?.email) ? event?.request?.clientMetadata?.debugUserId : null;
+    // const debugUserId = ['c@htic.io', 'blake@bertuccelli-booth.org'].includes(event?.request?.userAttributes?.email) ? event?.request?.clientMetadata?.debugUserId : null;
     event.response = {
         claimsOverrideDetails: {
             claimsToAddOrOverride: {
                 "https://hasura.io/jwt/claims": JSON.stringify({
                     "x-hasura-allowed-roles": ["user"],
                     "x-hasura-default-role": "user",
-                    "x-hasura-user-id": debugUserId ?? event.request.userAttributes.sub,
+                    "x-hasura-user-id": event.request.userAttributes.sub,
                 })
             }
         }
